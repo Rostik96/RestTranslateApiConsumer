52b54276f6c9d50562795fa6691e1ddcb3ca69b4
Rost <coderost@yandex.ru>
2023-01-06 08:32:33 +0300

    First POST-request by RestTemplate.

--- src/main/java/org/rost/study/Consumer.java
+++ src/main/java/org/rost/study/Consumer.java
@@ -1,7 +1,9 @@
 package org.rost.study;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -15,8 +17,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Consumer {
     public static void main( String[] args ) throws JsonProcessingException {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://reqres.in/api/users/2";
-        String response = restTemplate.getForObject(url, String.class);
+
+        final Map<String, String> jsonToSendMap = new HashMap<String, String>() {{
+            put("name", "testName");
+            put("job", "Test job");
+        }};
+        //Преобразуется в JSON
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonToSendMap);
+        String url = "https://reqres.in/api/users/";
+        String response = restTemplate.postForObject(url, request, String.class);
         System.out.println(new ObjectMapper().readTree(response).toPrettyString());
     }
 }
