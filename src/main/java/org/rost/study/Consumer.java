c971d09e2eeee956ecbe66529ac1072969d9f1c8
Rost <coderost@yandex.ru>
2023-01-06 08:21:47 +0300

    First REST API request by RestTemplate.

--- src/main/java/org/rost/study/Consumer.java
+++ src/main/java/org/rost/study/Consumer.java
@@ -1,11 +1,22 @@
 package org.rost.study;
 
+import java.util.Map;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Hello world!
  *
  */
 public class Consumer {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://reqres.in/api/users/2";
+        String response = restTemplate.getForObject(url, String.class);
+        System.out.println(new ObjectMapper().readTree(response).toPrettyString());
     }
 }
