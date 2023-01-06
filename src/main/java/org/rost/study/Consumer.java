52520a5201544e225bcc1e49eeacc0130a58e5cd
Rost <coderost@yandex.ru>
2023-01-06 12:43:08 +0300

    Refactor main-class

--- src/main/java/org/rost/study/Consumer.java
+++ src/main/java/org/rost/study/Consumer.java
@@ -1,14 +1,6 @@
 package org.rost.study;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Hello world!
@@ -16,16 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class Consumer {
     public static void main( String[] args ) throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-
-        final Map<String, String> jsonToSendMap = new HashMap<String, String>() {{
-            put("name", "testName");
-            put("job", "Test job");
-        }};
-        //Преобразуется в JSON
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonToSendMap);
-        String url = "https://reqres.in/api/users/";
-        String response = restTemplate.postForObject(url, request, String.class);
-        System.out.println(new ObjectMapper().readTree(response).toPrettyString());
+        Translator translator = new Translator();
+        System.out.println(translator.translate());
     }
 }
